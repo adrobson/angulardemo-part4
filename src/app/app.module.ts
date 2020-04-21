c6a80ed90bc9522fce21b6d1fb3d0dfe1f489943
Alistair Robson <adrobson@hotmail.com>
2020-04-21 20:55:01 +0100

    end of article 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { FundsHomeComponent } from './components/funds-home/funds-home.component';
+import { StocksHomeComponent } from './components/stocks-home/stocks-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavmenuComponent,
+    FundsHomeComponent,
+    StocksHomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
